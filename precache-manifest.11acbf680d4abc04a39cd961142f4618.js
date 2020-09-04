edc31726eca2c666b300527acd4070e6bc419387
Roberto Shimokawa <rluccaf@gmail.com>
2020-09-04 10:22:07 -0300

    Upload prd version

---
+++ precache-manifest.11acbf680d4abc04a39cd961142f4618.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e44becf17b3a978d94706caa47f6ba3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61ceff7248ecc98413c3",
+    "url": "/static/css/main.9d4c0275.chunk.css"
+  },
+  {
+    "revision": "91336b5f1adc7e3f89e7",
+    "url": "/static/js/2.25f4612a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.25f4612a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61ceff7248ecc98413c3",
+    "url": "/static/js/main.463ac5fd.chunk.js"
+  },
+  {
+    "revision": "c9337a9d38ae61497a6a",
+    "url": "/static/js/runtime-main.82d74f44.js"
+  },
+  {
+    "revision": "62c62e65122f8180e83db5c432b0e228",
+    "url": "/static/media/bethehero.62c62e65.png"
+  },
+  {
+    "revision": "09b65ad069d25738608eb71ee4c285d3",
+    "url": "/static/media/eficaz.09b65ad0.png"
+  },
+  {
+    "revision": "52a227fc044e77aee374f429567bfe94",
+    "url": "/static/media/huelma.52a227fc.png"
+  },
+  {
+    "revision": "c513b40656fa0d104e163dd23f069c36",
+    "url": "/static/media/myphoto.c513b406.png"
+  },
+  {
+    "revision": "80beea882705a3daa6eff07c29b10cf7",
+    "url": "/static/media/obra24horas.80beea88.png"
+  },
+  {
+    "revision": "3c8f6d58f7265b46bcb9296bd4a6578d",
+    "url": "/static/media/thankaparamedic.3c8f6d58.png"
+  }
+]);
